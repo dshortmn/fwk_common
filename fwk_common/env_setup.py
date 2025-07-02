4440e38e7ea8f09b28ea74ce7ff92f7168cff860
dshortmn <dshortmn@gmail.com>
2025-07-01 20:32:06 -0400

    Update

--- fwk_common/env_setup.py
+++ fwk_common/env_setup.py
@@ -12,23 +12,16 @@ def GetCurrentPath():
     return current_directory
 
 
-def get_caller_info():
-    """
-    Returns the absolute path and name of the module that called this function.
-    """
+def GetPathInfo():
     # inspect.stack()[1] gets the frame of the direct caller
     caller_frame = inspect.stack()[1]
-
     # Extract the filename from the frame information
     caller_filepath = caller_frame.filename
-
     # Make the path absolute
     absolute_path = path.abspath(caller_filepath)
-
-    # Get the module name (filename without extension)
-    module_name = path.splitext(path.basename(absolute_path))[0]
-
-    # Clean up the reference to the frame to avoid potential reference cycles
+    directory_path = path.dirname(absolute_path)
+    config_path = path.join(directory_path, "config")
+    sql_path = path.join(directory_path, "sql")
     del caller_frame
 
-    return absolute_path, module_name
+    return config_path, sql_path
