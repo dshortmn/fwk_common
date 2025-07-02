f565041d43745d24939e377f84e622b715ba91c2
dshortmn <dshortmn@gmail.com>
2025-07-01 22:48:20 -0400

    update

--- fwk_common/file_io.py
+++ fwk_common/file_io.py
@@ -5,9 +5,15 @@ def load_yaml(filename):
     """Load a YAML file and return its contents."""
     data = {}
     with open(filename, "r") as file:
-        for key, value in yaml.load(file, yaml.SafeLoader).iteritems():
-            print(f" {key=} , {value=}")
-            if data.get(key) is not None:
-                raise ValueError(f"Duplicate key found: {key}")
-            data[key] = value
+        data = yaml.load(file, yaml.SafeLoader)
+        if not isinstance(data, dict):
+            raise ValueError(f"YAML file {filename} does not contain a dictionary")
+        if not data:
+            raise ValueError(f"YAML file {filename} is empty")
+
+        # for key, value in yaml.load(file, yaml.SafeLoader).iteritems():
+        #    print(f" {key=} , {value=}")
+        #    if data.get(key) is not None:
+        #        raise ValueError(f"Duplicate key found: {key}")
+        #    data[key] = value
     return data
