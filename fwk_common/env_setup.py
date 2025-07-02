068c911caa3a415f15e5d27c704cdc6e3cd4dc8b
dshortmn <dshortmn@gmail.com>
2025-07-01 20:14:39 -0400

    currentpath

--- fwk_common/env_setup.py
+++ fwk_common/env_setup.py
@@ -4,3 +4,7 @@ from os import getcwd
 def GetCommonPath():
     current_directory = getcwd()
     return current_directory
+
+def GetCurrentPath():
+    current_directory = __file__ -- .rsplit('/', 2)[0]
+    return current_directory
