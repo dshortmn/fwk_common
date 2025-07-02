0f9b10b1dfa131d703aeeea05bf67a67e797733c
dshortmn <dshortmn@gmail.com>
2025-07-01 22:35:42 -0400

    update

---
+++ fwk_common/file_io.py
@@ -0,0 +1,13 @@
+import yaml
+
+
+def load_yaml(filename):
+    """Load a YAML file and return its contents."""
+    data = {}
+    with open(filename, "r") as file:
+        for key, value in yaml.load(file).iteritems():
+            print(f" {key=} , {value=}")
+            if data.get(key) is not None:
+                raise ValueError(f"Duplicate key found: {key}")
+            data[key] = value
+    return data
